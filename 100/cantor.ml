f83edb403c09683acf313c030e9a68ef5cb73fcc
jrh013 <jrh013@1eddc5a6-173b-11df-bff1-5f4ea15f2b7c>
2010-11-09 05:56:07 +0000

    Added a few cute Cantor theorem variants that I came across.

--- 100/cantor.ml
+++ 100/cantor.ml
@@ -31,3 +31,67 @@ let CANTOR = prove
     REWRITE_TAC[NOT_EXISTS_THM] THEN X_GEN_TAC `g:A->(A->bool)` THEN
     DISCH_THEN(MP_TAC o SPEC `\x:A. s(x) /\ ~(g x x)`) THEN
     REWRITE_TAC[SUBSET; IN; FUN_EQ_THM] THEN MESON_TAC[]]);;
+
+(* ------------------------------------------------------------------------- *)
+(* More explicit "injective" version as in Paul Taylor's book.               *)
+(* ------------------------------------------------------------------------- *)
+
+let CANTOR_THM_INJ' = prove
+ (`~(?f:(A->bool)->A. (!x y. f(x) = f(y) ==> x = y))`,
+  STRIP_TAC THEN
+  ABBREV_TAC `(g:A->A->bool) = \a. { b | !s. f(s) = a ==> b IN s}` THEN
+  MP_TAC(ISPEC `g:A->A->bool`
+   (REWRITE_RULE[NOT_EXISTS_THM] CANTOR_THM_SURJ)) THEN
+  FIRST_X_ASSUM(SUBST_ALL_TAC o SYM) THEN
+  ASM_REWRITE_TAC[EXTENSION; IN_ELIM_THM] THEN
+  ASM_MESON_TAC[]);;
+
+(* ------------------------------------------------------------------------- *)
+(* Another sequence of versions (Lawvere, Cantor, Taylor) taken from         *)
+(* http://ncatlab.org/nlab/show/Cantor%27s+theorem.                          *)
+(* ------------------------------------------------------------------------- *)
+
+let CANTOR_LAWVERE = prove
+ (`!h:A->(A->B).
+        (!f:A->B. ?x:A. h(x) = f) ==> !n:B->B. ?x. n(x) = x`,
+  REPEAT STRIP_TAC THEN
+  ABBREV_TAC `g:A->B = \a. (n:B->B) (h a a)` THEN
+  RULE_ASSUM_TAC(REWRITE_RULE[FUN_EQ_THM]) THEN
+  ASM_MESON_TAC[]);;
+
+let CANTOR = prove
+ (`!f:A->(A->bool). ~(!s. ?x. f x = s)`,
+  GEN_TAC THEN DISCH_THEN(MP_TAC o MATCH_MP CANTOR_LAWVERE) THEN
+  DISCH_THEN(MP_TAC o SPEC `(~)`) THEN MESON_TAC[]);;
+
+let CANTOR_TAYLOR = prove
+ (`!f:(A->bool)->A. ~(!x y. f(x) = f(y) ==> x = y)`,
+  REPEAT STRIP_TAC THEN
+  MP_TAC(ISPEC `\a:A.  { b:A | !s. f(s) = a ==> b IN s}`
+   (REWRITE_RULE[NOT_EXISTS_THM] CANTOR)) THEN
+  REWRITE_TAC[EXTENSION; IN_ELIM_THM] THEN
+  ASM_MESON_TAC[]);;
+
+let SURJECTIVE_COMPOSE = prove
+ (`(!y. ?x. f(x) = y) /\ (!z. ?y. g(y) = z) 
+   ==> (!z. ?x. (g o f) x = z)`,
+  MESON_TAC[o_THM]);;
+
+let INJECTIVE_SURJECTIVE_PREIMAGE = prove
+ (`!f:A->B. (!x y. f(x) = f(y) ==> x = y) ==> !t. ?s. {x | f(x) IN s} = t`,
+  REPEAT STRIP_TAC THEN
+  EXISTS_TAC `IMAGE (f:A->B) t` THEN
+  REWRITE_TAC[EXTENSION; IN_ELIM_THM; IN_IMAGE] THEN
+  ASM_MESON_TAC[]);;
+
+let CANTOR_JOHNSTONE = prove
+ (`!i:B->S f:B->S->bool.
+        ~((!x y. i(x) = i(y) ==> x = y) /\ (!s. ?z. f(z) = s))`,
+  REPEAT STRIP_TAC THEN
+  MP_TAC(ISPEC
+   `(IMAGE (f:B->S->bool)) o (\s:S->bool. {x | i(x) IN s})`
+    (REWRITE_RULE[NOT_EXISTS_THM] CANTOR)) THEN
+  REWRITE_TAC[] THEN MATCH_MP_TAC SURJECTIVE_COMPOSE THEN
+  ASM_REWRITE_TAC[SURJECTIVE_IMAGE] THEN
+  MATCH_MP_TAC INJECTIVE_SURJECTIVE_PREIMAGE THEN 
+  ASM_REWRITE_TAC[]);;
