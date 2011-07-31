54920ddb5634364963062e1893777819da0ab433
jrh013 <jrh013@1eddc5a6-173b-11df-bff1-5f4ea15f2b7c>
2011-07-31 18:53:50 +0000

    Added "Isabelle Light" from Petros Papapanagiotou and Jacques Fleuriot, an implementation of Isabelle-style tactics and other user-friendly shortcuts.

---
+++ IsabelleLight/isalight.ml
@@ -0,0 +1,19 @@
+(* ========================================================================= *)
+(*                              Isabelle Light                               *)
+(*   Isabelle/Procedural style additions and other user-friendly shortcuts.  *)
+(*                                                                           *)
+(*                   Petros Papapanagiotou, Jacques Fleuriot                 *)
+(*              Centre of Intelligent Systems and their Applications         *)
+(*                          University of Edinburgh                          *)
+(*                                 2009-2010                                 *)
+(* ========================================================================= *)
+(* FILE         : isahol.ml                                                  *)
+(* DESCRIPTION  : Main loader.                                               *)
+(* LAST MODIFIED: December 2010                                              *)
+(* ========================================================================= *)
+
+let paths = [".";"$/IsabelleLight"]
+in map (fun st -> load_on_path paths st)
+        ["support.ml";
+         "new_tactics.ml";
+         "meta_rules.ml"];;
