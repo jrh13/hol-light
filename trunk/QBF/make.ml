4a089547d0e2b449e50642e9ef1846d811b59361
jrh013 <jrh013@1eddc5a6-173b-11df-bff1-5f4ea15f2b7c>
2012-05-27 02:45:53 +0000

    Added Ondrej Kuncar's code for constructing proofs of quantified boolean formulas in HOL Light using proof traces from Squolem.

---
+++ trunk/QBF/make.ml
@@ -0,0 +1,10 @@
+(* ========================================================================= *)
+(* Ondrej Kuncar's HOL Light QBF code.                                       *)
+(* ========================================================================= *)
+
+#load "ocamlgraph/graph.cma";;
+#directory "+ocamlgraph";;
+loads "Minisat/make.ml";;
+loads "QBF/mygraph.ml";;
+loads "QBF/qbfr.ml";;
+loads "QBF/qbf.ml";;
