48669ca024faa53f27ec8494f39cacc49ece31a3
Oskar Abrahamsson <oskarabrahamsson@users.noreply.github.com>
2024-07-29 19:43:01 +0200

    Use new Double interface in metis

--- metis/waiting.ml
+++ metis/waiting.ml
@@ -2,23 +2,20 @@
 (* THE WAITING SET OF CLAUSES                                                *)
 (* ========================================================================= *)
 
-(* NOTE This code has been modified to use integer weights, and Num.num
-   instead of floats. *)
-
 module Waiting = struct
 
 (* ------------------------------------------------------------------------- *)
 (* A type of waiting sets of clauses.                                        *)
 (* ------------------------------------------------------------------------- *)
 
-type weight = Num.num;;
+type weight = Double.double;;
 
 type modelParameters = Model_parameters of {
   model : Model.parameters;
   initialPerturbations : int;
   maxChecks : int option;
   perturbations : int;
-  weight : int
+  weight : weight
 };;
 
 type parameters = Parameters of {
@@ -28,7 +25,7 @@ type parameters = Parameters of {
   modelsP : modelParameters list
 };;
 
-type distance = Num.num;;
+type distance = Double.double
 
 type waiting = Waiting of {
   parameters : parameters;
@@ -46,14 +43,14 @@ let defaultModels : modelParameters list = [
     initialPerturbations = 100;
     maxChecks = Some 20;
     perturbations = 0;
-    weight = 1
+    weight = Double.fromInt 1
   }];;
 
 let default : parameters =
   Parameters {
-    symbolsWeight = num_1;
-    literalsWeight = num_1;
-    variablesWeight = num_1;
+    symbolsWeight = Double.fromInt 1;
+    literalsWeight = Double.fromInt 1;
+    variablesWeight = Double.fromInt 1;
     modelsP = defaultModels
 };;
 
@@ -96,14 +93,16 @@ let initialModel axioms conjecture parm =
   m
 ;;
 
-let checkModels parms models (fv,cl) =
-  let check (parm,model) z =
-    let Model_parameters {maxChecks; weight} = parm in
-    let (vT,vF) = Model.check Model.interpretClause maxChecks model fv cl in
-    z */ ((num_1 +/ num_of_int vT // num_of_int (vT + vF)) **/
-          num_of_int weight) in
-  List.foldl check num_1 (zip parms models)
-;;
+let checkModels =
+  let one = Double.fromInt 1 in
+  fun parms models (fv,cl) ->
+    let check (parm,model) z =
+      let Model_parameters {maxChecks; weight} = parm in
+      let (vT,vF) = Model.check Model.interpretClause maxChecks model fv cl in
+      z *. (Double.pow (one +. Double.fromInt vT /. Double.fromInt (vT + vF))
+                       weight) in
+    List.foldl check one (zip parms models)
+  ;;
 
 let perturbModels parms models cls =
   let perturb (parm,model) =
@@ -116,27 +115,28 @@ let perturbModels parms models cls =
 (* Clause weights.                                                           *)
 (* ------------------------------------------------------------------------- *)
 
-let clauseSymbols cl = num_of_int (Literal.Set.typedSymbols cl);;
+let clauseSymbols cl = Double.fromInt (Literal.Set.typedSymbols cl);;
 
 let clauseVariables cl =
-  num_of_int (Name.Set.size (Literal.Set.freeVars cl) + 1);;
+  Double.fromInt (Name.Set.size (Literal.Set.freeVars cl) + 1);;
 
-let clauseLiterals cl = num_of_int (Literal.Set.size cl);;
+let clauseLiterals cl = Double.fromInt (Literal.Set.size cl);;
 
-let clausePriority cl =
-  num_of_int cl.Clause.Clause.id //
-  num_of_int 1_000_000_000_000;;
+let clausePriority =
+  let Some factor = Double.fromString "1e-12" in
+  fun cl ->
+    Double.fromInt cl.Clause.Clause.id *. factor;;
 
 let clauseWeight parm mods dist mcl cl =
   let Parameters {symbolsWeight; variablesWeight; literalsWeight;
                   modelsP} = parm in
   let lits = Clause.literals cl in
-  let symbolsW = clauseSymbols lits **/ symbolsWeight in
-  let variablesW = clauseVariables lits **/ variablesWeight in
-  let literalsW = clauseLiterals lits **/ literalsWeight in
+  let symbolsW = Double.pow (clauseSymbols lits) symbolsWeight in
+  let variablesW = Double.pow (clauseVariables lits) variablesWeight in
+  let literalsW = Double.pow (clauseLiterals lits) literalsWeight in
   let modelsW = checkModels modelsP mods mcl in
-  let weight = dist */ symbolsW */ variablesW */ literalsW */ modelsW in
-  let weight = weight +/ clausePriority cl in
+  let weight = dist *. symbolsW *. variablesW *. literalsW *. modelsW in
+  let weight = weight +. clausePriority cl in
   weight
 ;;
 
@@ -144,18 +144,10 @@ let clauseWeight parm mods dist mcl cl =
 (* Adding new clauses.                                                       *)
 (* ------------------------------------------------------------------------- *)
 
-let rat_sqrt s =
-  let rec sqrt n =
-    if n < 1 then s // num_2 else
-      let p = sqrt (n - 1) in
-      (p +/ (s // p)) // num_2 in
-  sqrt 5;;
-
 let add' waiting dist mcls cls =
   let Waiting {parameters; clauses; models} = waiting in
   let Parameters {modelsP} = parameters in
-  (* let dist = dist +. Math.ln (Real.fromInt (length cls)) in *)
-  let dist = dist +/ rat_sqrt (num_of_int (length cls)) in
+  let dist = dist +. Double.ln (Double.fromInt (length cls)) in
   let addCl (mcl,cl) acc =
     let weight = clauseWeight parameters models dist mcl cl in
     Heap.add acc (weight,(dist,cl)) in
@@ -172,7 +164,10 @@ let add waiting (dist,cls) =
     waiting
 ;;
 
-let cmp (w1,_) (w2,_) = Num.compare w1 w2;;
+let cmp (w1,_) (w2,_) =
+  if Double.(<) w1 w2 then Less
+  else if Double.(>) w1 w2 then Greater
+  else Equal;;
 
 let empty parameters axioms conjecture =
   let Parameters {modelsP} = parameters in
@@ -181,14 +176,16 @@ let empty parameters axioms conjecture =
   Waiting {parameters = parameters; clauses = clauses; models = models}
 ;;
 
-let newWaiting parameters (Ax_cj.Ax_cj_cl {axioms_cl; conjecture_cl}) =
-  let mAxioms = mkModelClauses axioms_cl
-  and mConjecture = mkModelClauses conjecture_cl in
-  let waiting = empty parameters mAxioms mConjecture in
-  if List.null axioms_cl && List.null conjecture_cl then
-    waiting
-  else
-    add' waiting num_0 (mAxioms @ mConjecture) (axioms_cl @ conjecture_cl)
+let newWaiting =
+  let zero = Double.fromInt 0 in
+  fun parameters (Ax_cj.Ax_cj_cl {axioms_cl; conjecture_cl}) ->
+    let mAxioms = mkModelClauses axioms_cl
+    and mConjecture = mkModelClauses conjecture_cl in
+    let waiting = empty parameters mAxioms mConjecture in
+    if List.null axioms_cl && List.null conjecture_cl then
+      waiting
+    else
+      add' waiting zero (mAxioms @ mConjecture) (axioms_cl @ conjecture_cl)
 ;;
 
 (* ------------------------------------------------------------------------- *)
